fb428a9a6f2539fa3374f61b04b7a36a90ef2da7
JIIIIIND <jinwoo700@gmail.com>
2019-07-26 00:37:05 +0900

    MainUI 기초 작업 완료

---
+++ MoneyProject/Assets/Scripts/UI/PopUp.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUp : MonoBehaviour
+{
+
+    [SerializeField] GameObject PopUpPanel;
+    void Start()
+    {
+        
+    }
+
+    public void ClickUI()
+    {
+        if (PopUpPanel.activeInHierarchy)
+            PopUpPanel.SetActive(false);
+        else
+            PopUpPanel.SetActive(true);
+    }
+}
