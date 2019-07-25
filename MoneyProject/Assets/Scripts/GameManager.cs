d183b7f19ecd1547314453b9cc4d8d9dd4503ec9
DESKTOP-1TQDUKI\amthf <aythffk@gmail.com>
2019-07-26 02:35:00 +0900

    GameManager 싱글톤으로 전환 get, set 추가 turnCount 변수 추가

--- MoneyProject/Assets/Scripts/GameManager.cs
+++ MoneyProject/Assets/Scripts/GameManager.cs
@@ -7,8 +7,24 @@ public class GameManager : MonoBehaviour
     private int enlightenmentLevel;
     private int dangerLevel;
     private int trustLevel;
+    private int turnCount;
     private int money;
     private Resource[] resource; // 분리하는게 나을지도?
+
+    private static GameManager instance;
+
+    public static GameManager getInstance()
+    {
+        return instance;
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(this.gameObject);
+    }
 
     void Start()
     {
@@ -19,5 +35,40 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void nextTurn()
+    {
+
+    }
+
+    public int EnlightenmentLevel
+    {
+        get { return enlightenmentLevel; }
+        set { enlightenmentLevel = value; }
+    }
+
+    public int DangerLevel
+    {
+        get { return dangerLevel; }
+        set { dangerLevel = value; }
+    }
+
+    public int TrustLevel
+    {
+        get { return dangerLevel; }
+        set { dangerLevel = value; }
+    }
+
+    public int TurnCount
+    {
+        get { return dangerLevel; }
+        set { dangerLevel = value; }
+    }
+
+    public int Money
+    {
+        get { return money; }
+        set { money = value; }
     }
 }
