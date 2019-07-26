86bf189c3d722e375427f1985a7193cee7206ea1
rockpell <aythffk@gmail.com>
2019-07-27 02:37:03 +0900

    프롤로그, 메인씬 추가

---
+++ MoneyProject/Assets/Scripts/MainScene.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainScene : MonoBehaviour
+{
+    [SerializeField] GameObject startButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("activeButton", 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void startGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Prolog");
+    }
+
+    private void activeButton()
+    {
+        startButton.SetActive(true);
+    }
+}
