e5c8c929ebed2b66ed5fb7164d4d8cdb1a3ff61c
JIIIIIND <jinwoo700@gmail.com>
2019-07-26 20:22:13 +0900

    캘린더 버그 수정, 턴당 7일 지나도록 수정

--- MoneyProject/Assets/Scripts/Calendar.cs
+++ MoneyProject/Assets/Scripts/Calendar.cs
@@ -11,8 +11,11 @@ public class Calendar
     
     public void nextDay()
     {
-        ++day;
-        day = checkMonth(month, day);
+        for(int i = 0; i<7; i++)
+        {
+            ++day;
+            day = checkMonth(month, day);
+        }
     }
 
     private int checkMonth(int month, int day)
@@ -21,52 +24,89 @@ public class Calendar
         switch (month)
         {
             case 1:
-                if (day > 30)
+                if (day > 31)
+                {
                     _result = 1;
+                    ++this.month;
+                }
                 break;
             case 2:
-                if (day > 30)
+                if (day > 28)
+                {
                     _result = 1;
+                    ++this.month;
+                }
                 break;
             case 3:
-                if (day > 30)
+                if (day > 31)
+                {
                     _result = 1;
+                    ++this.month;
+                }
                 break;
             case 4:
                 if (day > 30)
+                {
                     _result = 1;
+                    ++this.month;
+                }
                 break;
             case 5:
-                if (day > 30)
+                if (day > 31)
+                {
                     _result = 1;
+                    ++this.month;
+                }
                 break;
             case 6:
                 if (day > 30)
+                {
                     _result = 1;
+                    ++this.month;
+                }
                 break;
             case 7:
-                if (day > 30)
+                if (day > 31)
+                {
                     _result = 1;
+                    ++this.month;
+                }
                 break;
             case 8:
-                if (day > 30)
+                if (day > 31)
+                {
                     _result = 1;
+                    ++this.month;
+                }
                 break;
             case 9:
                 if (day > 30)
+                {
                     _result = 1;
+                    ++this.month;
+                }
                 break;
             case 10:
-                if (day > 30)
+                if (day > 31)
+                {
                     _result = 1;
+                    ++this.month;
+                }
                 break;
             case 11:
                 if (day > 30)
+                {
                     _result = 1;
+                    ++this.month;
+                }
                 break;
             case 12:
-                if (day > 30)
+                if (day > 31)
+                {
                     _result = 1;
+                    this.month = 1;
+                    ++this.year;
+                }
                 break;
         }
         return _result;
