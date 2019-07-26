4e48cf3bc796b7b9aa741fb487b3172e1e2baac9
JIIIIIND <jinwoo700@gmail.com>
2019-07-26 16:07:47 +0900

    경로 활성화 코드 작성 중

---
+++ MoneyProject/Assets/Scripts/HighlightPath.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighlightPath : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> paths;
+    void Start()
+    {
+        
+    }
+
+    public void TurnOnPath(string start, string end)
+    {
+        Debug.Log(paths.Find(x => x.name == start + "-" + end));
+    }
+    public void TurnOffPath(string start, string end)
+    {
+        Debug.Log(paths.Find(x => x.name == start + "-" + end));
+    }
+    void Update()
+    {
+        
+    }
+}
