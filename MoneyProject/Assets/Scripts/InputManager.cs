e20f8b9fffb6bc0d7e138187b6c9286074c9ebf4
rockpell <aythffk@gmail.com>
2019-07-26 13:03:35 +0900

    마우스 드래그 및 휠로 카메라 조정 기능 추가

--- MoneyProject/Assets/Scripts/InputManager.cs
+++ MoneyProject/Assets/Scripts/InputManager.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
+    [SerializeField] private Transform[] boundarys; // right, left, upper, bottom
     [SerializeField] private List<Station> nowPath;
+    [SerializeField] private float zoomScale = 0.5f;
+    [SerializeField] private float dragScale = 0.05f;
+
     private Worker nowWorker;
 
-    private static InputManager instance;
+    private Vector3 dragPivot;
+    private float xMin, xMax;
+    private float yMin, yMax;
+    
 
+    private static InputManager instance;
     public static InputManager getInstance()
     {
         return instance;
@@ -25,6 +33,10 @@ public class InputManager : MonoBehaviour
     void Start()
     {
         nowPath = new List<Station>();
+        xMax = boundarys[0].position.x;
+        xMin = boundarys[1].position.x;
+        yMax = boundarys[2].position.y;
+        yMin = boundarys[3].position.y;
     }
 
     // Update is called once per frame
@@ -34,6 +46,24 @@ public class InputManager : MonoBehaviour
         {
             Debug.Log(checkObject());
         }
+        if(Input.GetMouseButtonDown(1))
+        {
+            dragPivot = Input.mousePosition;
+        }
+        else if (Input.GetMouseButton(1))
+        {
+            Vector3 _drag = (Input.mousePosition - dragPivot) * dragScale * 0.01f;
+
+            Camera.main.transform.position += _drag;
+        }
+
+        float x = Mathf.Clamp(Camera.main.transform.position.x, xMin, xMax);
+        float y = Mathf.Clamp(Camera.main.transform.position.y, yMin, yMax);
+        Camera.main.transform.position = new Vector3(x, y, Camera.main.transform.position.z);
+
+        Camera.main.orthographicSize -= Input.mouseScrollDelta.y * zoomScale;
+        if (Camera.main.orthographicSize > 5) Camera.main.orthographicSize = 5;
+        else if (Camera.main.orthographicSize < 1) Camera.main.orthographicSize = 1;
     }
 
     private GameObject checkObject()
@@ -51,7 +81,6 @@ public class InputManager : MonoBehaviour
                 select.GetComponent<Station>().addNowPath();
                 Debug.Log("Station Add");
             }
-                
         }
         else if(raycast.collider != null && raycast.collider.tag == "Worker")
         {
@@ -84,7 +113,7 @@ public class InputManager : MonoBehaviour
             
         return select;
     }
-    public void addNowPath(Station station)
+    public void addNowPath(Station station) // 연결 가능한 거점인지 확인한 후에 add 해주는 작업 필요
     {
         nowPath.Add(station);
     }
@@ -103,4 +132,5 @@ public class InputManager : MonoBehaviour
     {
         nowWorker = worker;
     }
+
 }
