e80083dabcfa45de0cc7224b2817a411827fb3e5
DESKTOP-1TQDUKI\amthf <aythffk@gmail.com>
2019-07-26 09:32:28 +0900

    프롤로그 씬 추가

---
+++ MoneyProject/Assets/Scripts/UI/Prolog.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Prolog : MonoBehaviour
+{
+    [SerializeField] GameObject startButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("activeButton", 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void startGame()
+    {
+        SceneManager.LoadScene("SampleUI");
+    }
+
+    private void activeButton()
+    {
+        startButton.SetActive(true);
+    }
+}
